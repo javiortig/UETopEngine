946fe74140d62faa5b5011bf25a258362173701c
javiortig <dev.javiog@gmail.com>
2020-12-10 14:16:24 +0100

    firstv2

---
+++ Source/TopEngine/TopEngine.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TopEngine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopEngine, "TopEngine" );
---
+++ Source/TopEngine/TopEngineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TopEngineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPENGINE_API ATopEngineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
